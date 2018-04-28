6138f8169775906691d972bb73d7a90375e2ba0f
GeorgeDonev <georgesdonev@gmail.com>
2018-04-27 23:03:59 -1000

    added a 'get' request returning a json object message string

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,24 @@
 console.log('Hello')
+// import the correspoding modules
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined')) // use "combined" to print out logs in a certain way
+app.use(bodyParser.json())
+app.use(cors())
+
+// put requests here:
+
+// handling endpoint /status ; when request is recived,
+// func sends back json object with attribute message: and sting 'hello world'
+app.get('/status', (req, res) => { // request and response
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+// runs the app
+app.listen(process.env.PORT || 8081) // environment port if avalible, or 8081
